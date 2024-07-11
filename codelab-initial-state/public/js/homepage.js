356fd394c75717c18eea51fff339c4cd324b1aec
WyldMann <118483265+WyldMann@users.noreply.github.com>
2024-07-11 18:18:22 +0800

    Initial commit

--- codelab-initial-state/public/js/homepage.js
+++ codelab-initial-state/public/js/homepage.js
@@ -28,6 +28,13 @@ export async function onDocumentReady(firebaseApp) {
   const auth = firebaseApp.auth();
   const db = firebaseApp.firestore();
 
+  //point to the local emulator
+
+  if (location.hostname === "127.0.0.1") {
+    console.log("127.0.0.1 detected!");
+    auth.useEmulator("http://127.0.0.1:9099");
+    db.useEmulator("127.0.0.1", 8080);
+
   const homePage = new HomePage(db, auth);
   mount(document.body, homePage);
 }
@@ -170,6 +177,13 @@ class HomePage {
   }
 
   addToCart(id, itemData) {
+
+    //log in check
+    if (this.auth.currentUser === null) {
+      this.showError("You must be signed in!");
+      return;
+    }
+
     console.log("addToCart", id, JSON.stringify(itemData));
     return this.db
       .collection("carts")
@@ -192,4 +206,5 @@ class HomePage {
   showError(e) {
     alert(e);
   }
+}
 }
